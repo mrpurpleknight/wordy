d27279eacd5d8fdf3c7a49b06d5ab368199bb510
purpleknight <gabriele.schincaglia@student.supsi.ch>
2021-01-02 03:14:32 +0100

    generalize completed

--- lib/screens/random_word_screen.dart
+++ lib/screens/random_word_screen.dart
@@ -1,7 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:learn_words/constants.dart';
-import 'package:learn_words/widgets/random_word_tile.dart';
+import 'package:learn_words/providers/word.dart';
+import 'package:learn_words/widgets/word_tile.dart';
 import 'package:learn_words/widgets/tool_bar.dart';
+import 'package:provider/provider.dart';
 
 class RandomWordScreen extends StatefulWidget {
   @override
@@ -13,11 +17,13 @@ class _RandomWordScreenState extends State<RandomWordScreen>
   @override
   Widget build(BuildContext context) {
     super.build(context);
+    Size size = MediaQuery.of(context).size;
+    Future<Word> wordFuture = Provider.of<Future<Word>>(context, listen: false);
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
-              colors: [gradientBegin, gradientMed, gradientEnd ],
+              colors: [gradientBegin, gradientMed, gradientEnd],
               begin: Alignment.topCenter,
               end: Alignment.bottomCenter),
         ),
@@ -25,11 +31,43 @@ class _RandomWordScreenState extends State<RandomWordScreen>
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 40, left: 40, right: 40, bottom: 25),
-              child: RandomWordTile(),
-            ),
-
+            FutureBuilder<Word>(
+                future: wordFuture,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Padding(
+                        padding: const EdgeInsets.only(
+                            top: 40, left: 40, right: 40, bottom: 25),
+                        child: Container(
+                            padding: EdgeInsets.only(top: 50, right: 25, left: 25, bottom: 25),
+                            height: size.height * 0.6,
+                            width: size.width * 0.9,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black12.withOpacity(0.3),
+                                  spreadRadius: 2,
+                                  blurRadius: 2,
+                                  offset: Offset(2, 4), // changes position of shadow
+                                ),
+                              ],
+                            ),
+                            child: Center(child: SpinKitWave(
+                              color: Colors.black.withOpacity(0.7),
+                              type: SpinKitWaveType.center,
+                              size: 35.0,
+                            )),),);
+                  }
+                  else {
+                    return Padding(
+                      padding: const EdgeInsets.only(
+                          top: 40, left: 40, right: 40, bottom: 25),
+                      child: WordTile(snapshot.data),
+                    );
+                  }
+                }),
             ToolBar(),
           ],
         ),
---
+++ lib/widgets/word_tile.dart
@@ -0,0 +1,70 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:learn_words/providers/word.dart';
+import 'package:provider/provider.dart';
+
+class WordTile extends StatelessWidget {
+  final Word _word;
+
+  WordTile(this._word);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      padding: EdgeInsets.only(top: 50, right: 25, left: 25, bottom: 25),
+      height: size.height * 0.6,
+      width: size.width * 0.9,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12.withOpacity(0.3),
+            spreadRadius: 2,
+            blurRadius: 2,
+            offset: Offset(2, 4), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(_word.name, style: Theme.of(context).textTheme.headline2),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 10),
+            child: Text(
+              _word.partOfSpeech,
+              style: Theme.of(context).textTheme.headline3,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 15, bottom: 35),
+            child: TyperAnimatedTextKit(
+              text: [
+                '1. ${_word.definition}',
+              ],
+              textStyle: Theme.of(context).textTheme.headline3,
+              repeatForever: false,
+              speed: Duration(milliseconds: 35),
+              totalRepeatCount: 1,
+            ),
+          ),
+          (_word.examplePhrase == null)
+              ? Text('')
+              : TyperAnimatedTextKit(
+                  text: [
+                    _word.examplePhrase,
+                  ],
+                  textStyle: Theme.of(context).textTheme.headline4,
+                  repeatForever: false,
+                  speed: Duration(milliseconds: 40),
+                  totalRepeatCount: 1,
+                )
+        ],
+      ),
+    );
+  }
+}
