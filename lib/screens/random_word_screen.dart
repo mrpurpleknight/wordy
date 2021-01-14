2898486ef255cc84fdc3152a1efe4612040f92be
purpleknight <gabriele.schincaglia@student.supsi.ch>
2021-01-14 13:10:43 +0100

    added connectivity and fixed loop bug

--- lib/screens/random_word_screen.dart
+++ lib/screens/random_word_screen.dart
@@ -32,7 +32,7 @@ class _RandomWordScreenState extends State<RandomWordScreen>
         child: MultiProvider(
           providers: [
             Provider(
-              create: (_) =>RandomWordManager.instance.getRandomWord(),
+              create: (_) => RandomWordManager.instance.getRandomWord(),
               lazy: false,
             ),
             Provider(
